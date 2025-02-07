4c7c900f6f1b0f516c463c25795b2664f3e52f6e
Khai-08 <khalil.aparecio0308@gmail.com>
2025-02-07 13:38:18 +0800

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -2,6 +2,7 @@
 
 use CodeIgniter\Router\RouteCollection;
 
+use App\Controllers\UserController;
 use App\Controllers\PagesController;
 
 $routes->get('/', 'PagesController::index');
@@ -9,3 +10,6 @@ $routes->get('/', 'PagesController::index');
 // Pages Route/s
 $routes->get('pages', [PagesController::class, 'index']);
 $routes->get('(:segment)', [PagesController::class, 'view']);
+
+$routes->post('api/login', [UserController::class,'loginUser']);
+
