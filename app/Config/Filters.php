873416fbfa4bf0ebb0d5313dc4bde0b9877a3f1d
Khai-08 <khalil.aparecio0308@gmail.com>
2025-02-07 13:38:07 +0800

    Update Filters.php

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -11,14 +11,8 @@ use CodeIgniter\Filters\SecureHeaders;
 
 class Filters extends BaseConfig
 {
-    /**
-     * Configures aliases for Filter classes to
-     * make reading things nicer and simpler.
-     *
-     * @var array<string, class-string|list<class-string>> [filter_name => classname]
-     *                                                     or [filter_name => [classname1, classname2, ...]]
-     */
     public array $aliases = [
+        'oauthfilter'   => \App\Filters\OAuthFilter::class,
         'csrf'          => CSRF::class,
         'toolbar'       => DebugToolbar::class,
         'honeypot'      => Honeypot::class,
@@ -26,12 +20,6 @@ class Filters extends BaseConfig
         'secureheaders' => SecureHeaders::class,
     ];
 
-    /**
-     * List of filter aliases that are always
-     * applied before and after every request.
-     *
-     * @var array<string, array<string, array<string, string>>>|array<string, list<string>>
-     */
     public array $globals = [
         'before' => [
             // 'honeypot',
@@ -45,29 +33,11 @@ class Filters extends BaseConfig
         ],
     ];
 
-    /**
-     * List of filter aliases that works on a
-     * particular HTTP method (GET, POST, etc.).
-     *
-     * Example:
-     * 'post' => ['foo', 'bar']
-     *
-     * If you use this, you should disable auto-routing because auto-routing
-     * permits any HTTP method to access a controller. Accessing the controller
-     * with a method you don't expect could bypass the filter.
-     *
-     * @var array<string, list<string>>
-     */
+
     public array $methods = [];
 
-    /**
-     * List of filter aliases that should run on any
-     * before or after URI patterns.
-     *
-     * Example:
-     * 'isLoggedIn' => ['before' => ['account/*', 'profiles/*']]
-     *
-     * @var array<string, array<string, list<string>>>
-     */
-    public array $filters = [];
+    public array $filters = [
+        'oauthfilter' => ['before' => ['blog', 'blog/*']]
+
+    ];
 }
